3a7e7da50f001e4e3cde2ac4780c11328036f17c
ishikav <65843008+ishikav@users.noreply.github.com>
2020-06-19 20:41:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var names=["priya","Jessica","Krishna","James","jishnu","Seetha","Latha","John","Joker","Neethu"];
+var names=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 for (var name in names){
 	if (names[name].startsWith("J")||names[name].startsWith("j")){
 		console.log("Good Bye "+names[name]);
